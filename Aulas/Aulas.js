2c8e3d2695ff3ef0808a228d94233401eacd6c38
MarcusVogado <marcus_620@hotmail.com>
2023-11-16 21:35:22 -0300

    upgrade

--- Aulas/Aulas.js
+++ Aulas/Aulas.js
@@ -1,10 +1,1 @@
-let matriz = []
 
-matriz[0] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-matriz[1] = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]    
-matriz[2] = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-let coluna = 0
-for (let j = 0; j < matriz.length; j++) {
-    valorTotal+=matriz[j][coluna]  
-}
-console.log(valorTotal)
