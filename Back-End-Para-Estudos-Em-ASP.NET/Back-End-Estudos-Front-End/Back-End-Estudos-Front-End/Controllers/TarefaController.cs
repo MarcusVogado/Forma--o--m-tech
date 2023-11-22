ba43b3de323f1282ebe43cacce20d500fac6d575
MarcusVogado <marcus_620@hotmail.com>
2023-11-22 11:33:04 -0300

    update Condicionais/ Aulas teste API

--- Back-End-Para-Estudos-Em-ASP.NET/Back-End-Estudos-Front-End/Back-End-Estudos-Front-End/Controllers/TarefaController.cs
+++ Back-End-Para-Estudos-Em-ASP.NET/Back-End-Estudos-Front-End/Back-End-Estudos-Front-End/Controllers/TarefaController.cs
@@ -11,22 +11,88 @@ namespace Back_End_Estudos_Front_End.Controllers
         [HttpGet]
         public IActionResult ListaDeTarefas()
         {
+            var tarefas = GetListaTarefas();
+
+            return new JsonResult(new { tarefas = tarefas })
+            {
+                StatusCode = 200,
+                ContentType = "application/json"
+            };
+        }
+
+        public static List<Tarefa> GetListaTarefas()
+        {
             var tarefas = new List<Tarefa>()
             {
-                new Tarefa { Id = 1, Description="Estudar Programação",IsCompleted=false},
-                new Tarefa { Id = 2, Description="Estudar Inglês",IsCompleted=false},
-                new Tarefa { Id = 3, Description="Criar uma API"}
+                new Tarefa
+                {
+                    Id = 1,
+                    Description="Estudar JavaScript",
+                    IsCompleted=false,
+                    DaysAndTimes = new DaysAndTimes[]{
+                       new DaysAndTimes{DayOfWeek = DayOfWeek.Friday, StartTime=new TimeSpan(09,30,0), EndTime=new TimeSpan(11,30,0)},
+                       new DaysAndTimes{DayOfWeek = DayOfWeek.Tuesday,StartTime= new TimeSpan(09,30,0), EndTime=new TimeSpan(11,30,0)},
+                       new DaysAndTimes{DayOfWeek= DayOfWeek.Thursday, StartTime=new TimeSpan(16,30,0), EndTime=new TimeSpan(18,30,0)}
+                    },
+                },
+                new Tarefa
+                {
+                        Id = 2,
+                        Description="Estudar Inglês",
+                        IsCompleted=false,
+                        DaysAndTimes = new DaysAndTimes[] {
+                           new DaysAndTimes{DayOfWeek=DayOfWeek.Friday, StartTime=new TimeSpan(08,0,0) , EndTime=new TimeSpan(9,10,0)},
+                           new DaysAndTimes {DayOfWeek=DayOfWeek.Tuesday,StartTime= new TimeSpan(08,0,0),EndTime= new TimeSpan(9,10,0)},
+                           new DaysAndTimes {DayOfWeek= DayOfWeek.Thursday,StartTime= new TimeSpan(13,30,0),EndTime= new TimeSpan(14,30,0)}
+                        },
+                },
+                new Tarefa
+                {
+                       Id = 3,
+                       Description = "Estudar HTML",
+                       IsCompleted = true,
+                       DaysAndTimes = new DaysAndTimes[]{
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Friday, StartTime = new TimeSpan(13,0,0),EndTime= new TimeSpan(14,10,0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Tuesday, StartTime = new TimeSpan(13, 0, 0),EndTime= new TimeSpan(14, 10, 0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Thursday,StartTime = new TimeSpan(14, 40, 0),EndTime= new TimeSpan(16, 0, 0)}
+                       }
+                },
+                new Tarefa
+                {
+                       Id = 3,
+                       Description = "Estudar Algoritmos",
+                       IsCompleted = true,
+                       DaysAndTimes = new DaysAndTimes[]{
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Friday, StartTime = new TimeSpan(13,0,0),EndTime= new TimeSpan(14,10,0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Tuesday, StartTime = new TimeSpan(13, 0, 0),EndTime= new TimeSpan(14, 10, 0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Thursday,StartTime = new TimeSpan(14, 40, 0),EndTime= new TimeSpan(16, 0, 0)}
+                       }
+                },
+                new Tarefa
+                {
+                       Id = 4,
+                       Description = "Estudar CSS",
+                       IsCompleted = true,
+                       DaysAndTimes = new DaysAndTimes[]{
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Sunday, StartTime = new TimeSpan(13,0,0),EndTime= new TimeSpan(14,10,0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Wednesday, StartTime = new TimeSpan(13, 0, 0),EndTime= new TimeSpan(14, 10, 0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Friday,StartTime = new TimeSpan(14, 40, 0),EndTime= new TimeSpan(16, 0, 0)}
+                       }
+                },
+                new Tarefa
+                {
+                       Id = 5,
+                       Description = "Aulas Ímã Tech",
+                       IsCompleted = true,
+                       DaysAndTimes = new DaysAndTimes[]{
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Sunday, StartTime = new TimeSpan(19,0,0),EndTime= new TimeSpan(21,0,0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Wednesday, StartTime = new TimeSpan(19, 0, 0),EndTime= new TimeSpan(21, 0, 0)},
+                          new DaysAndTimes {DayOfWeek= DayOfWeek.Saturday,StartTime = new TimeSpan(19, 0, 0),EndTime= new TimeSpan(21, 0, 0)}
+                       }
+                }
             };
-
-
-            Response.ContentType = "application/json";
-            Response.StatusCode = 200;            
-            return Ok(new JsonResult(tarefas)
-            {
-                ContentType = "application/json",
-                StatusCode = 200,
-                SerializerSettings = true,
-            });
+            return tarefas;
         }
     }
 }
+
--- Back-End-Para-Estudos-Em-ASP.NET/Back-End-Estudos-Front-End/Back-End-Estudos-Front-End/Models/Tarefa.cs
+++ Back-End-Para-Estudos-Em-ASP.NET/Back-End-Estudos-Front-End/Back-End-Estudos-Front-End/Models/Tarefa.cs
@@ -3,8 +3,19 @@
     public class Tarefa
     {
         public int Id { get; set; }
-        public string Description { get; set; } =string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DaysAndTimes[] DaysAndTimes { get; set; } = null!;
         public bool IsCompleted { get; set; }
 
     }
+
+
+    public class DaysAndTimes
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+    }
+
 }
