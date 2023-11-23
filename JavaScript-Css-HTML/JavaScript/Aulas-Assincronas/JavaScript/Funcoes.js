1c5746e67a7a15644e1dc0fd2591784f69dd6701
MarcusVogado <marcus_620@hotmail.com>
2023-11-23 03:52:07 -0300

    Iniciando TypeScript

---
+++ JavaScript-Css-HTML/JavaScript/Aulas-Assincronas/JavaScript/Funcoes.js
@@ -0,0 +1,11 @@
+//Funções
+//Funções são um bloc de código que vai realizar uma ação 
+function sum(a,b){
+    return a+b
+}
+const sumValue = sum(10,2)
+console.log(sumValue)
+
+const sumArrow = (a,b)=> a+b;
+const sumArrowValue=sumArrow(10,20)
+console.log(sumArrowValue)
